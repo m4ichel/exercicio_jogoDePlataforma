7bbc414b2249456fb914a5515679a248ba594553
Lucas Michel <lucas.michel@sou.inteli.edu.br>
2025-03-03 21:23:40 -0300

    debug off

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const config =
             arcade: 
             {
                 gravity:  { y: 1000 },
-                debug: true,
+                debug: false,
             }
         },
 
